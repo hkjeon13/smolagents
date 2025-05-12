439afd8fc01f06332d623907555f214abd730e74
psyche <hkjeon13@gmail.com>
2025-05-12 17:54:00 +0900

    FIX ERROR for the Agents

--- src/smolagents/default_tools.py
+++ src/smolagents/default_tools.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-
+import textwrap
 # Copyright 2024 The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -84,8 +84,7 @@ class FinalAnswerTool(Tool):
     output_type = "any"
 
     def forward(self, answer: Any) -> Any:
-        return answer
-
+        return repr(textwrap.dedent(answer))
 
 class UserInputTool(Tool):
     name = "user_input"
