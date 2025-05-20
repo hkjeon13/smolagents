117b1ac6194593c750435f05f192ebf642043c4d
psyche <hkjeon13@gmail.com>
2025-05-20 13:20:59 +0900

    refactoring: all

--- src/smolagents/cli.py
+++ src/smolagents/cli.py
@@ -18,7 +18,8 @@ import argparse
 import os
 
 from dotenv import load_dotenv
-from smolagents import CodeAgent, InferenceClientModel, LiteLLMModel, Model, OpenAIServerModel, Tool, TransformersModel
+from smolagents.models import InferenceClientModel, LiteLLMModel, Model, OpenAIServerModel, Tool, TransformersModel
+from smolagents.agents import CodeAgent
 from smolagents.default_tools import TOOL_MAPPING
 
 leopard_prompt = "How many seconds would it take for a leopard at full speed to run through Pont des Arts?"
